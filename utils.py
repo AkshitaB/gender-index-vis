3937db90e27060c85cb445031ef46aef86680f42
ajosulaa <akhilajosyula@gmail.com>
2019-04-24 02:37:12 -0400

    health data, margins fixed

--- utils.py
+++ utils.py
@@ -104,67 +104,79 @@ class DataReader:
         This only considers indicators that fall in the 0-100 range.
         '''
         indicator_column_map = {}
-        indicator_column_map['FTE employment rate (%)'] = ['FTE employment rate (%) W', 'FTE employment rate (%) M']
-        indicator_column_map['Employed in education, human health and social work (%)'] = ['Employed people in education, human health and social work activities (%) W', \
+        indicator_column_map['FTE employment rate'] = ['FTE employment rate (%) W', 'FTE employment rate (%) M']
+        indicator_column_map['Employed in education,health,social'] = ['Employed people in education, human health and social work activities (%) W', \
                                                                                        'Employed people in education, human health and social work activities (%) M']
-        indicator_column_map['Ability to take time off during working hours for personal or family matters (%)'] = ['Ability to take one hour or two off during working hours to take care of personal or family matters (%) W', \
+        indicator_column_map['Personal time off'] = ['Ability to take one hour or two off during working hours to take care of personal or family matters (%) W', \
                                                                                                                 'Ability to take one hour or two off during working hours to take care of personal or family matters (%) M']
-        indicator_column_map['Career Prospects Index (points, 0-100)'] = ['Career Prospects Index (points, 0-100) W', \
+        indicator_column_map['Career Prospects Index'] = ['Career Prospects Index (points, 0-100) W', \
                                                                           'Career Prospects Index (points, 0-100) M']
-        indicator_column_map['Not at-risk-of-poverty (%)'] = ['Not at-risk-of-poverty (%) W', 'Not at-risk-of-poverty (%) M']
-        indicator_column_map['Income distribution S20/S80 (%)'] = ['Income distribution S20/S80 (%) W', 'Income distribution S20/S80 (%) M']
-        indicator_column_map['Graduates of tertiary education (%)'] = ['Graduates of tertiary education (%) W', 'Graduates of tertiary education (%) M']
-        indicator_column_map['People participating in formal or non-formal education (%)'] = ['People participating in formal or non-formal education (%) W', \
+        indicator_column_map['Not at-risk-of-poverty'] = ['Not at-risk-of-poverty (%) W', 'Not at-risk-of-poverty (%) M']
+        indicator_column_map['Income distribution'] = ['Income distribution S20/S80 (%) W', 'Income distribution S20/S80 (%) M']
+        indicator_column_map['Graduates of tertiary education'] = ['Graduates of tertiary education (%) W', 'Graduates of tertiary education (%) M']
+        indicator_column_map['Participating in education'] = ['People participating in formal or non-formal education (%) W', \
                                                                                               'People participating in formal or non-formal education (%) M']
-        indicator_column_map['Tertiary students in education, health and welfare, humanities and arts (%)'] = ['Tertiary students in education, health and welfare, humanities and arts (%) W',
+        indicator_column_map['Tertiary students in education,health'] = ['Tertiary students in education, health and welfare, humanities and arts (%) W',
                                                                                                                'Tertiary students in education, health and welfare, humanities and arts (%) M']
-        indicator_column_map['Caring for children, elderly, or people with disabilities, every day (%)'] = ['People caring for and educating their children or grandchildren, elderly or people with disabilities, every day (%) W',\
+        indicator_column_map['Caring for children, elderly'] = ['People caring for and educating their children or grandchildren, elderly or people with disabilities, every day (%) W',\
                                                                                                             'People caring for and educating their children or grandchildren, elderly or people with disabilities, every day (%) M']
 
-        indicator_column_map['Daily cooking and/or household work (%)'] = ['People doing cooking and/or household, every day (%) W',
+        indicator_column_map['Cooking/household work'] = ['People doing cooking and/or household, every day (%) W', \
                                                                            'People doing cooking and/or household, every day (%) M']
 
-        indicator_column_map['Weekly participation in sporting, cultural, leisure activities (%)'] = ['Workers doing sporting, cultural or leisure activities outside of their home, at least daily or several times a week (%) W',\
+        indicator_column_map['Participation in sports, leisures'] = ['Workers doing sporting, cultural or leisure activities outside of their home, at least daily or several times a week (%) W',\
                                                                                                   'Workers doing sporting, cultural or leisure activities outside of their home, at least daily or several times a week (%) M']
 
-        indicator_column_map['Ministers (%)'] = ['Share of ministers (%) W', 'Share of ministers (%) M']
-        indicator_column_map['Members of parliament (%)'] = ['Share of members of parliament (%) W', 'Share of members of parliament (%) M']
-        indicator_column_map['Members of regional assemblies (%)'] = ['Share of members of regional assemblies (%) W', 'Share of members of regional assemblies (%) W']
-        indicator_column_map['Board members in companies (%)'] = ['Share of members of boards in largest quoted companies, supervisory board or board of directors (%) W',\
+        indicator_column_map['Ministers'] = ['Share of ministers (%) W', 'Share of ministers (%) M']
+        indicator_column_map['Parliament'] = ['Share of members of parliament (%) W', 'Share of members of parliament (%) M']
+        indicator_column_map['Regional assemblies'] = ['Share of members of regional assemblies (%) W', 'Share of members of regional assemblies (%) W']
+        indicator_column_map['Companies'] = ['Share of members of boards in largest quoted companies, supervisory board or board of directors (%) W', \
                                                               'Share of members of boards in largest quoted companies, supervisory board or board of directors (%) M']
-        indicator_column_map['Board members of central bank (%)'] = ['Share of board members of central bank (%) W', 'Share of board members of central bank (%) M']
-        indicator_column_map['Board members of research funding organisations (%)'] = ['Share of board members of research funding organisations (%) W', 'Share of board members of research funding organisations (%) M']
-        indicator_column_map['Board members of broadcasting organisations (%)'] = ['Share of board members of publically owned broadcasting organisations (%)  W', 'Share of board members of publically owned broadcasting organisations (%)  M']
-        indicator_column_map['Decision makers in national Olympic sport organisations (%)'] = ['Share of members of highest decision making body of the national Olympic sport organisations (%)  W', 'Share of members of highest decision making body of the national Olympic sport organisations (%)  M']
+        indicator_column_map['Central bank'] = ['Share of board members of central bank (%) W', 'Share of board members of central bank (%) M']
+        indicator_column_map['Research funding organisations'] = ['Share of board members of research funding organisations (%) W', 'Share of board members of research funding organisations (%) M']
+        indicator_column_map['Broadcasting organisations'] = ['Share of board members of publically owned broadcasting organisations (%)  W', \
+                                                               'Share of board members of publically owned broadcasting organisations (%)  M']
+        indicator_column_map['Olympic sport organisations'] = ['Share of members of highest decision making body of the national Olympic sport organisations (%)  W', \
+                                                               'Share of members of highest decision making body of the national Olympic sport organisations (%)  M']
 
+        indicator_column_map['Self-perceived health'] = ['Self-perceived health, good or very good (%) W', 'Self-perceived health, good or very good (%) M'] 
+        indicator_column_map['Life expectency'] = ['Life expectancy at birth (years) W','Life expectancy at birth (years) M']
+        indicator_column_map['Healthy life years'] = ['Healthy life years at birth (years) W','Healthy life years at birth (years) M']
+        indicator_column_map['Not Smoking, Drinking'] = ['People who don\'t smoke and are not involved in harmful drinking (%) W',\
+                                                          'People who don\'t smoke and are not involved in harmful drinking (%) M']
+        indicator_column_map['Physically active, healthy food'] = ['People doing physical activities and/or consuming fruits and vegetables (%) W', \
+                                                                    'People doing physical activities and/or consuming fruits and vegetables (%) M']
+        indicator_column_map['Unmet medical health'] = ['Population without unmet needs for medical examination (%) W', 'Population without unmet needs for medical examination (%) M']
+        indicator_column_map['Unmet dental health'] = ['Population without unmet needs for dental examination (%) W', 'Population without unmet needs for dental examination (%) M']
 
         indicators = {}
-        indicators['WORK'] = ['FTE employment rate (%)',
-                              'Employed in education, human health and social work (%)',
-                              'Ability to take time off during working hours for personal or family matters (%)',
-                              'Career Prospects Index (points, 0-100)']
+        indicators['WORK'] = ['FTE employment rate',
+                              'Employed in education,health,social',
+                              'Personal time off',
+                              'Career Prospects Index']
 
-        indicators['MONEY'] = ['Not at-risk-of-poverty (%)',
-                               'Income distribution S20/S80 (%)']
+        indicators['MONEY'] = ['Not at-risk-of-poverty',
+                               'Income distribution']
 
-        indicators['KNOWLEDGE'] = ['Graduates of tertiary education (%)',
-                                   'People participating in formal or non-formal education (%)',
-                                   'Tertiary students in education, health and welfare, humanities and arts (%)']
+        indicators['KNOWLEDGE'] = ['Graduates of tertiary education',
+                                   'Participating in education',
+                                   'Tertiary students in education,health']
 
-        indicators['TIME'] = ['Caring for children, elderly, or people with disabilities, every day (%)',
-                              'Daily cooking and/or household work (%)',
-                              'Weekly participation in sporting, cultural, leisure activities (%)']
+        indicators['TIME'] = ['Caring for children, elderly',
+                              'Cooking/household work',
+                              'Participation in sports, leisures']
 
-        indicators['POWER'] = ['Ministers (%)', \
-                               'Members of parliament (%)', \
-                               'Members of regional assemblies (%)', \
-                               'Board members in companies (%)', \
-                               'Board members of central bank (%)', \
-                               'Board members of research funding organisations (%)', \
-                               'Board members of broadcasting organisations (%)',\
-                               'Decision makers in national Olympic sport organisations (%)'
+        indicators['POWER'] = ['Ministers', \
+                               'Parliament', \
+                               'Regional assemblies', \
+                               'Companies', \
+                               'Central bank', \
+                               'Research funding organisations', \
+                               'Broadcasting organisations',\
+                               'Olympic sport organisations'
                                ]
 
+        indicators['HEALTH'] = ['Self-perceived health','Life expectency','Healthy life years','Not Smoking, Drinking','Physically active, healthy food', 'Unmet medical health','Unmet dental health']
 
         self.indicators = indicators
         self.indicator_column_map = indicator_column_map
